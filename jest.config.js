200dde200dbcd43b11da08f131724ad48210168f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-02 17:50:17 -0400

    Config jest to find files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  modulePaths: ["/shared/vendor/modules"],
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules", "bower_components", "shared"],
+
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",

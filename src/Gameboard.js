49bc4bc7f0c7072d75fb0bdd1d04231c6bdb0633
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 18:37:00 -0400

    Write skeleton for pseudocode and add gameBoard props

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,10 +1,21 @@
 /* Factory that returns a gameboard that can place ships with Ship(), recieve attacks based on coords 
    and then decides whether to hit() if ship is in that spot, records hits and misses, and reports if
    all its ships have been sunk. */
-const Gameboard = () => {
-  const newBoard = {};
+const Gameboard = (mapX = 10, mapY = 10) => {
+  const newBoard = {
+    occupiedCells: [],
+    addShip: null,
+    recieveAttack: null,
+    misses: [],
+    hits: [],
+  };
 
-  // Set up board
+  // Method for adding a ship at a given coords in given direction if ship will fit on board
+  addShip = (coords, direction) => {
+    // Are coords already in occupiedCells?
+    // Will the ship fit in the desired direction?
+    // Add the correct cells to occupied cells
+  };
 
   return newBoard;
 };

3c9ef4b25199bfb40e1f41d332ed659752d4339b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 17:13:17 -0400

    Test that gameboard default map is 10x10

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -5,4 +5,12 @@ describe("Basic Functionality", () => {
     const testBoard = Gameboard();
     expect(testBoard).toBeInstanceOf(Object);
   });
+
+  test("Gameboard has a default size of 10x10", () => {
+    const testBoard = Gameboard();
+    expect(testBoard.map).toHaveLength(10);
+    for (let i = 0; i < testBoard.map.length; i += 1) {
+      expect(testBoard.map[i].length).toHaveLength(10);
+    }
+  });
 });

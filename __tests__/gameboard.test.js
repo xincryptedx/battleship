2c40088d36e8f16ac293d1890155794cb3abffa7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 19:17:01 -0400

    Remove tests

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -1,24 +1,8 @@
 import Gameboard from "../src/Gameboard";
-import Ship from "../src/Ship";
 
 describe("Basic Functionality", () => {
   test("Gameboard returns an object", () => {
     const testBoard = Gameboard();
     expect(testBoard).toBeInstanceOf(Object);
-  });
-
-  test("Gameboard.addShip returns valid ship", () => {
-    const testBoard = Gameboard();
-    expect(testBoard.addShip()).toBeInstanceOf(Ship);
-  });
-
-  test("Gameboard.addShip adds the correct cells to occupiedCells", () => {
-    const testBoard = Gameboard();
-    const testViper = testBoard.addShip([4, 4], "N", 3);
-    expect(testBoard.occupiedCells).toEqual(
-      { position: [4, 4], ship: testViper },
-      { position: [4, 3], ship: testViper },
-      { position: [4, 2], ship: testViper }
-    );
   });
 });

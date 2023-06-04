ab3530884059cc0c8a78ad72d652d5ca2cf4a258
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 19:04:45 -0400

    Create and return new ship

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,5 @@
+import Ship from "./Ship";
+
 /* Factory that returns a gameboard that can place ships with Ship(), recieve attacks based on coords 
    and then decides whether to hit() if ship is in that spot, records hits and misses, and reports if
    all its ships have been sunk. */
@@ -12,9 +14,11 @@ const Gameboard = (mapX = 10, mapY = 10) => {
 
   // Method for adding a ship at a given coords in given direction if ship will fit on board
   newBoard.addShip = (coords, direction, shipTypeIndex) => {
+    const newShip = Ship(shipTypeIndex);
     // Are coords already in occupiedCells?
     // Will the ship fit in the desired direction?
     // Add the correct cells to occupied cells
+    return newShip;
   };
 
   return newBoard;

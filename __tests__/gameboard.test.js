8a2f64a2f421853a3c2c35356e3c343af96d0309
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 19:36:01 -0400

    Fix test expect to be an array

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -11,6 +11,8 @@ describe("addOccupiedCell", () => {
   test("testBoard occupiedCells gets correct cell added to it", () => {
     const testBoard = Gameboard();
     testBoard.addOccupiedCell([4, 4], 3);
-    expect(testBoard.occupiedCells).toEqual({ position: [4, 4], shipType: 3 });
+    expect(testBoard.occupiedCells).toEqual([
+      { position: [4, 4], shipType: 3 },
+    ]);
   });
 });

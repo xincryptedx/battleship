4d67a0821c7ba566f6a7c9a86d316ff47bd63de0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 17:05:14 -0400

    Create sekeleton for Gameboard factory

---
+++ src/Gameboard.js
@@ -0,0 +1,12 @@
+/* Factory that returns a gameboard that can place ships with Ship(), recieve attacks based on coords 
+   and then decides whether to hit() if ship is in that spot, records hits and misses, and reports if
+   all its ships have been sunk. */
+const Gameboard = () => {
+  const newBoard = {};
+
+  // Set up board
+
+  return newBoard;
+};
+
+export default Gameboard;

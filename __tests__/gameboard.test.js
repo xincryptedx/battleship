89200eee272d1d6a7345d428ce49ca5751f1ebea
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 19:31:33 -0400

    Test that addOccupiedCell adds new cell

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -1,8 +1,16 @@
 import Gameboard from "../src/Gameboard";
 
-describe("Basic Functionality", () => {
+describe("General Functionality", () => {
   test("Gameboard returns an object", () => {
     const testBoard = Gameboard();
     expect(testBoard).toBeInstanceOf(Object);
+  });
+});
+
+describe("addOccupiedCell", () => {
+  test("testBoard occupiedCells gets correct cell added to it", () => {
+    const testBoard = Gameboard();
+    testBoard.addOccupiedCell([4, 4], 3);
+    expect(testBoard.occupiedCells).toEqual({ position: [4, 4], shipType: 3 });
   });
 });

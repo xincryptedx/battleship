fab9f9ef768ee324f877cbd7a2c218d9d6ef5f34
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 15:59:10 -0400

    Add ship names based on size

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,12 @@
+// Contains the names for the ships based on size
+const shipNames = {
+  2: "Sentinel Probe",
+  a3: "Assault Titan",
+  b3: "Viper Mech",
+  4: "Iron Goliath",
+  5: "Leviathan",
+};
+
 // Factory that can create and return one of a variety of pre-determined ships.
 const Ship = () => {
   // Create the ship object that will be returned

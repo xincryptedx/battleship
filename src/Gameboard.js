cb83355f215d5d44905cef0372a66b2e159f453b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 19:35:14 -0400

    Remove currently unused params

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,7 +3,7 @@ import Ship from "./Ship";
 /* Factory that returns a gameboard that can place ships with Ship(), recieve attacks based on coords 
    and then decides whether to hit() if ship is in that spot, records hits and misses, and reports if
    all its ships have been sunk. */
-const Gameboard = (mapX = 10, mapY = 10) => {
+const Gameboard = () => {
   const newBoard = {
     occupiedCells: [],
     addOccupiedCell: null,

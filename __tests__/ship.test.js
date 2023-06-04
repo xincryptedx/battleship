4e19ede5323d51c2338960da3b3d637ea69f36be
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-04 16:13:21 -0400

    Test for ship index 1 name prop

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -5,4 +5,9 @@ describe("Basic Functionality", () => {
     const testShip = Ship();
     expect(testShip).toBeInstanceOf(Object);
   });
+
+  test("Ship(1) returns an object with name prop:'Sentinel Probe'", () => {
+    const testShip = Ship(1);
+    expect(testShip.name).toBe("Sentinel Probe");
+  });
 });

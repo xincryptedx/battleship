7ad07b60aef97802b8fbf0ef92c0ffc9538143ee
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-05 19:15:46 -0400

    Remove redundant test

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -13,10 +13,4 @@ describe("addShip", () => {
     testBoard.addShip(2);
     expect(testBoard.ships).toHaveLength(1);
   });
-
-  test("a valid ship is added, judging by type value", () => {
-    const testBoard = Gameboard();
-    testBoard.addShip(2);
-    expect(testBoard.ships[0].type).toBe("Assault Titan");
-  });
 });

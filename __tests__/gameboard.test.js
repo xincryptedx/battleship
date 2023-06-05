6aa4cfe0d4b10c1e0e06e6f05641aa34bf9b82d4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-05 19:28:35 -0400

    Test that neg coord ships not added

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -13,4 +13,10 @@ describe("addShip", () => {
     testBoard.addShip(2);
     expect(testBoard.ships).toHaveLength(1);
   });
+
+  test("ships w/ negative coordinates will not be added", () => {
+    const testBoard = Gameboard();
+    testBoard.addShip(2, [0, 0], "N");
+    expect(testBoard.ships).toHaveLength(0);
+  });
 });

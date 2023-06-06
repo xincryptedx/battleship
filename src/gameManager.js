314561e9423062a56ef27dc220b20cac096003c4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 15:44:03 -0400

    Add game manager module file

---
+++ src/gameManager.js
@@ -0,0 +1,3 @@
+/* This module holds the game loop logic for starting games, creating
+   required objects, iterating through turns, reporting game outcome when
+   a player loses, and restart the game */

993dfbb69c8b10aaf7c7524981cffc475b3a7f0c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 15:02:13 -0400

    Write validation helper method for attacks

--- src/Player.js
+++ src/Player.js
@@ -16,6 +16,30 @@ const Player = () => {
     sendAttack: null,
   };
 
+  // Helper method for validating that attack position is on board
+  const validatePosition = (position, gameboard) => {
+    // Does gameboard exist with maxBoardX/y properties?
+    if (!gameboard || !gameboard.maxBoardX || !gameboard.maxBoardY) {
+      return false;
+    }
+    // Is position constrained to maxboardX/Y and both are ints in an array?
+    if (
+      position &&
+      Array.isArray(position) &&
+      position.length === 2 &&
+      Number.isInteger(position[0]) &&
+      Number.isInteger(position[2]) &&
+      position[0] >= 0 &&
+      position[0] <= gameboard.maxBoardX &&
+      position[1] >= 0 &&
+      position[1] <= gameboard.maxBoardY
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  // Method for sending attack to rival gameboard
   thisPlayer.sendAttack = (position, gameboard = thisPlayer.gameboard) => {
     // Stuff
   };

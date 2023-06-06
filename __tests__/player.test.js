ea4854bc11bcfa5ee4972b89a4222f0e48133f2c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 14:20:52 -0400

    Test that falsy values return

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -14,9 +14,15 @@ describe("Player Name", () => {
     expect(testPlayer.name).toBe("string");
   });
 
-  test("player.name = anything else should use that things .toString()", () => {
+  test("player.name = other types should use that objects .toString()", () => {
     const testPlayer = Player();
     testPlayer.name = 123;
     expect(testPlayer.name).toBe("123");
+  });
+
+  test("player.name = falsy values not '' returns ''", () => {
+    const testPlayer = Player();
+    testPlayer.name = null;
+    expect(testPlayer.name).toBe("");
   });
 });

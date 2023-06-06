ea5a7152a08cabebb1ceee2c142b7efdb3265588
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 16:27:18 -0400

    Add basic game loop pseudocode

--- src/gameManager.js
+++ src/gameManager.js
@@ -3,10 +3,20 @@
    a player loses, and restart the game */
 const gameManager = () => {
   // Initialization
+  /* -Create user gameboard
+     -Create AI gameboard
+     -Create user player
+     -Create AI player
+     -Set rivalBoard properties */
   // Start a game
+  /* -Have user add ships 
+     -Have AI add ships */
+  // Determine if game is over after every turn
   // Iterate between players for attacks
-  // Determine if game is over
-  // Report game results
+  /* Determine who goes first with a coinflip
+     -Query user for attack on their turns
+     -Query AI for attack on AI turns */
+  // Report game results if game ends
   // Restart the game
 };
 

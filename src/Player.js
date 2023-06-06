97d94e770dc0e2f7bd763fbd465eb5be26354e8e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 14:01:16 -0400

    Write basic Player factory

--- src/Player.js
+++ src/Player.js
@@ -1,1 +1,11 @@
 /* Factory that creates and returns a player object that can take a shot at opponent's game board */
+const Player = () => {
+  const newPlayer = {
+    name: "",
+    gameboard: null,
+  };
+
+  return newPlayer;
+};
+
+export default Player;

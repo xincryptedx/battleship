75bc24baeba153a1dc2265e73cca5cd89357483e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-06 16:18:18 -0400

    Add basic skeleton for game manager

--- src/gameManager.js
+++ src/gameManager.js
@@ -1,3 +1,13 @@
 /* This module holds the game loop logic for starting games, creating
    required objects, iterating through turns, reporting game outcome when
    a player loses, and restart the game */
+const gameManager = () => {
+  // Initialization
+  // Start a game
+  // Iterate between players for attacks
+  // Determine if game is over
+  // Report game results
+  // Restart the game
+};
+
+export default gameManager;

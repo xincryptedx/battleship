ec5dbba586d423cd268adb776f418564f738956b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-05 20:02:39 -0400

    Consolidate conditions to one if and test direction

--- src/Ship.js
+++ src/Ship.js
@@ -58,15 +58,19 @@ const Ship = (index, position, direction) => {
   };
 
   // Use position and direction to add occupied cells coords
-  if (Array.isArray(position) && position.length === 2) {
-    if (Number.isInteger(position[0]) && Number.isInteger(position[1])) {
-      for (let i = 0; i < newShip.size; i += 1) {
-        const newCoords = [
-          position[0] + i * directionIterator[direction][0],
-          position[1] + i * directionIterator[direction][1],
-        ];
-        newShip.occupiedCells.push(newCoords);
-      }
+  if (
+    Array.isArray(position) &&
+    position.length === 2 &&
+    Number.isInteger(position[0]) &&
+    Number.isInteger(position[1]) &&
+    Object.keys(directionIterator).includes(direction)
+  ) {
+    for (let i = 0; i < newShip.size; i += 1) {
+      const newCoords = [
+        position[0] + i * directionIterator[direction][0],
+        position[1] + i * directionIterator[direction][1],
+      ];
+      newShip.occupiedCells.push(newCoords);
     }
   }
 

18e8c08194d5daeedde93c98e8b7f73db34032c1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-08 18:52:13 -0400

    Import web interface, and events for testing

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import "./style/reset.css";
 import "./style/style.css";
 import gameManager from "./modules/gameManager";
 import canvasManager from "./modules/canvasManager";
+import webInterface from "./modules/webInterface";
+
+// import events for testing in dev tools
+import events from "./modules/events";
+
+window.events = events;
+
+events.emit("hideAll");

5ea86a4faf9080ddd6d048a2c94f4672b3cd89b0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-07 21:20:12 -0400

    Add more pseudocode to finish outline of module

--- src/modules/webInterface.js
+++ src/modules/webInterface.js
@@ -1,4 +1,4 @@
-/* This module has two primary functions:
+/* This module has three primary functions:
    1. Get ship placement coordinates from the user based on their clicks on the web interface
    2. Get attack placement coordinates from the user based on the same
    3. Other minor interface actions such as handling button clicks (start game, restart, etc) */
@@ -23,12 +23,6 @@ const webInterface = (() => {
   // #endregion
 
   // #region Ship placement section
-  /* Methods that ask the user to supply coords
-     and a direction to use for the gameboard's addShip method by clicking on a div.
-     Then check that the ship was added to the gameboard, and if it was move on 
-     to the next one. If it was not, ask for the placement of the same ship again.
-     Move on after all 5 player ships have been placed, one of each */
-
   // Show the ship placement UI
   const showPlacement = () => {
     // Hide all then show proper elements by adding a class
@@ -37,11 +31,13 @@ const webInterface = (() => {
   // Check if all ships placed
   const checkShipPlacement = () => {
     // Check what ships have been placed using module scope variable
-    // Call showGame
+    // Call showGame if all ships have been placed
   };
 
+  // Handle clicks on the ship placement grid
   const handlePlacementClick = () => {
-    // Respond to a click on the placement interface
+    // Send an even trying to place the ship
+    // Check the ship placement so the current ship for placement is updated properly
   };
 
   // #endregion
@@ -50,6 +46,11 @@ const webInterface = (() => {
   // Show the game UI
   const showGame = () => {
     // Hide all then show the game section elements
+  };
+
+  // Handle clicks on the enemy
+  const handleAttackClick = () => {
+    // Send event that will attempt to send an attack based on clicked cell
   };
   // #endregion
 })();

a55181c1b177e8b8480b2d8d8ca4d568b5432941
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-07 22:47:06 -0400

    Add module for creating canvas elements

---
+++ src/modules/canvasManager.js
@@ -0,0 +1,9 @@
+import gridCanvas from "../helpers/gridCanvas";
+
+/* This module creates canvas elements and adds them to the appropriate 
+   places in the DOM. It also draws symbols for hits or misses. */
+const canvasManager = (() => {
+  // Replace the three grid placeholder elements with the proper canvases
+})();
+
+export default canvasManager;

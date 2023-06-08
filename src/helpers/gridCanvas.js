597feeb6104ddfd6f5b8d7cd4e9b5a508c00e16e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-07 23:47:13 -0400

    Add event and handler for mouse move highlight

--- src/helpers/gridCanvas.js
+++ src/helpers/gridCanvas.js
@@ -1,4 +1,5 @@
 function createGridCanvas(sizeX, sizeY) {
+  // #region Create the canvas element and draw grid
   const canvas = document.createElement("canvas");
   canvas.width = sizeX;
   canvas.height = sizeY;
@@ -29,10 +30,14 @@ function createGridCanvas(sizeX, sizeY) {
     ctx.stroke();
   }
 
-  // Add and handle event for canvas clicks
+  // #endregion
+
+  // #region Add event handlers for clicks, mousemove, and mouseleave
+  // Set the cell size refs
   const cellSizeX = canvas.width / 10; // Width of each cell
   const cellSizeY = canvas.height / 10; // Height of each cell
 
+  // Add and handle event for canvas clicks
   const handleClick = (event) => {
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
@@ -44,6 +49,45 @@ function createGridCanvas(sizeX, sizeY) {
     console.log(`x: ${clickedCellX}, y: ${clickedCellY}`);
   };
   canvas.addEventListener("click", handleClick);
+
+  // Add and handle event for mousemove
+  function handleMousemove(event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    const hoveredCellX = Math.floor(mouseX / cellSizeX);
+    const hoveredCellY = Math.floor(mouseY / cellSizeY);
+
+    // Apply hover effect to the hovered cell
+    // const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+
+    // Draw the grid
+    for (let x = 0; x < 10; x += 1) {
+      for (let y = 0; y < 10; y += 1) {
+        const cellX = x * cellSizeX;
+        const cellY = y * cellSizeY;
+
+        if (x === hoveredCellX && y === hoveredCellY) {
+          // Apply hover effect to the hovered cell
+          ctx.fillStyle = "gray"; // Set a different color for the hovered cell
+          ctx.fillRect(cellX, cellY, cellSizeX, cellSizeY);
+        } else {
+          // Draw the regular cells
+          ctx.fillStyle = "lightgray"; // Set the color for regular cells
+          ctx.fillRect(cellX, cellY, cellSizeX, cellSizeY);
+        }
+
+        // Draw grid lines
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(cellX, cellY, cellSizeX, cellSizeY);
+      }
+    }
+  }
+  canvas.addEventListener("mousemove", handleMousemove);
+
+  // #endregion
 
   return canvas;
 }

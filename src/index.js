7883feca4bd0ab6a72c62adf360e5cbde746ad94
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-08 16:30:38 -0400

    Add css reset rules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import "./style/reset.css";
 import "./style/style.css";
 import gameManager from "./modules/gameManager";
 import canvasManager from "./modules/canvasManager";

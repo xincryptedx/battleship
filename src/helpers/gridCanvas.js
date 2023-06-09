296ebb966cf80039f1b7017102c9710caaca2337
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-09 17:21:04 -0400

    Move logic for dawing lines to method

--- src/helpers/gridCanvas.js
+++ src/helpers/gridCanvas.js
@@ -56,27 +56,32 @@ const createGridCanvas = (sizeX, sizeY, options) => {
   // Set transparent background
   ctx.clearRect(0, 0, sizeX, sizeY);
 
-  // Draw grid lines
-  const gridSize = Math.min(sizeX, sizeY) / 10;
-  const lineColor = "black";
-  ctx.strokeStyle = lineColor;
-  ctx.lineWidth = 1;
+  const drawLines = () => {
+    // Draw grid lines
+    const gridSize = Math.min(sizeX, sizeY) / 10;
+    const lineColor = "black";
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = 1;
 
-  // Draw vertical lines
-  for (let x = 0; x <= sizeX; x += gridSize) {
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, sizeY);
-    ctx.stroke();
-  }
+    // Draw vertical lines
+    for (let x = 0; x <= sizeX; x += gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, sizeY);
+      ctx.stroke();
+    }
 
-  // Draw horizontal lines
-  for (let y = 0; y <= sizeY; y += gridSize) {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(sizeX, y);
-    ctx.stroke();
-  }
+    // Draw horizontal lines
+    for (let y = 0; y <= sizeY; y += gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(sizeX, y);
+      ctx.stroke();
+    }
+  };
+
+  // Draw original lines
+  drawLines();
 
   // #endregion
 

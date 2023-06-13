fdd7929e27970625e1ba0ac93dcebadae216c2af
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-13 19:01:36 -0400

    Remove events and events log modules

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@ import gameManager from "./modules/gameManager";
 import canvasManager from "./modules/canvasManager";
 import webInterface from "./modules/webInterface";
 
-// import events for testing in dev tools
-import events from "./modules/events";
-// Import events logger
-import eventsLogger from "./helpers/eventLogger";
-
-eventsLogger();
-window.events = events;
-
 // Initialize modules
 gameManager();
 webInterface();

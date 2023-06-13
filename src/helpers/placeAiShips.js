4fbd6d249de0b478828c792b06aa1783ac4b2cd2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-13 16:19:46 -0400

    Add methods for getting ai ships

--- src/helpers/placeAiShips.js
+++ src/helpers/placeAiShips.js
@@ -5,6 +5,22 @@ import events from "../modules/events";
 
 // This helper will attempt to add ships to the ai gameboard in a variety of ways for varying difficulty
 const placeAiShips = () => {
+  // Copy of the ai ships array and method to get it
+  let shipsCopy = [];
+  const setAiShips = (ships) => {
+    // Erase old ships data
+    shipsCopy = [];
+    ships.forEach((ship) => {
+      shipsCopy.push(ship);
+    });
+    // Emit event signalling ships have been copied and are ready for use
+    events.emit("aiShipsSet");
+  };
+
+  // Method that requests information about current user ships using event
+  const requestAiShips = () => {
+    events.emit("requestAiShips");
+  };
   // Place a ship randomly until one successfully placed
   // Place a ship along edges until one successfully placed
   // Place a ship based on quadrant

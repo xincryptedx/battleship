5968e1092463a2019a0f7db4c714eee18fb6fc64
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-13 16:33:21 -0400

    Add grid height and width consts

--- src/helpers/placeAiShips.js
+++ src/helpers/placeAiShips.js
@@ -4,7 +4,11 @@ import events from "../modules/events";
 */
 
 // This helper will attempt to add ships to the ai gameboard in a variety of ways for varying difficulty
-const placeAiShips = () => {
+const placeAiShips = (difficulty = 0) => {
+  // Grid size
+  const gridHeight = 10;
+  const gridWidth = 10;
+
   // Copy of the ai ships array and method to get it
   let shipsCopy = [];
   const setAiShips = (ships) => {
@@ -21,7 +25,12 @@ const placeAiShips = () => {
   const requestAiShips = () => {
     events.emit("requestAiShips");
   };
+
   // Place a ship randomly until one successfully placed
+  const placeRandomShip = () => {
+    // Get random coordinates
+    // Try the placement
+  };
   // Place a ship along edges until one successfully placed
   // Place a ship based on quadrant
   // Combine placement tactics to create varying difficulties

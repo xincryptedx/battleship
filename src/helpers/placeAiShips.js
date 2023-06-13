026faa591e37f78acfc85d2c602c58f5cd2026fa
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-13 16:16:26 -0400

    Add module skeleton for placing ai ships

---
+++ src/helpers/placeAiShips.js
@@ -0,0 +1,14 @@
+import events from "../modules/events";
+/* Events Subbed:
+
+*/
+
+// This helper will attempt to add ships to the ai gameboard in a variety of ways for varying difficulty
+const placeAiShips = () => {
+  // Place a ship randomly until one successfully placed
+  // Place a ship along edges until one successfully placed
+  // Place a ship based on quadrant
+  // Combine placement tactics to create varying difficulties
+};
+
+export default placeAiShips;

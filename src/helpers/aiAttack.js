18c3b029b28d7e90796d42d634f72268e007975d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-14 16:06:12 -0400

    Get random coordinates on grid

--- src/helpers/aiAttack.js
+++ src/helpers/aiAttack.js
@@ -1,4 +1,10 @@
 // This helper will look at current hits and misses and then return an attack
 const aiAttack = (rivalBoard) => {
+  const gridHeight = 10;
+  const gridWidth = 10;
   const { hits, misses } = rivalBoard;
+
+  // Method for returning random attack
+  const x = Math.floor(Math.random() * gridWidth);
+  const y = Math.floor(Math.random() * gridHeight);
 };

edd1bb6f0b040f64639e132ccb685b839583c1f5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-14 16:07:45 -0400

    For now return random attack

--- src/helpers/aiAttack.js
+++ src/helpers/aiAttack.js
@@ -3,8 +3,12 @@ const aiAttack = (rivalBoard) => {
   const gridHeight = 10;
   const gridWidth = 10;
   const { hits, misses } = rivalBoard;
+  let attackCoords = [];
 
   // Method for returning random attack
   const x = Math.floor(Math.random() * gridWidth);
   const y = Math.floor(Math.random() * gridHeight);
+  attackCoords = [x, y];
+
+  return attackCoords;
 };

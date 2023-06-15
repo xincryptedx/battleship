bc87c18505430113c1c282cd02b08fcbfba0676a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-15 16:27:06 -0400

    Write simple methods for erase and append

--- src/modules/gameLog.js
+++ src/modules/gameLog.js
@@ -1,3 +1,18 @@
-const gameLog = () => {};
+const gameLog = () => {
+  // Ref to log text
+  const logText = document.querySelector(".log-text");
+
+  // Erase the log text
+  const erase = () => {
+    logText.textContent = "";
+  };
+
+  // Add to log text
+  const append = (stringToAppend) => {
+    if (stringToAppend) {
+      logText.textContent += stringToAppend.toString();
+    }
+  };
+};
 
 export default gameLog;

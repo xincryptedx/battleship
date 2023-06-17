f6ddc8fd44d5d23ff55c72a68d8a80a965fa75d9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-17 13:11:08 -0400

    Configure loader to work with webpack

--- src/helpers/imageLoader.js
+++ src/helpers/imageLoader.js
@@ -1,39 +1,29 @@
-const fs = require("fs");
-const path = require("path");
-
 const imageLoader = () => {
   const imageRefs = {
-    sp: { hit: [], attack: [], gen: [] },
-    at: { hit: [], attack: [], gen: [] },
-    vm: { hit: [], attack: [], gen: [] },
-    ig: { hit: [], attack: [], gen: [] },
-    l: { hit: [], attack: [], gen: [] },
+    SP: { hit: [], attack: [], gen: [] },
+    AT: { hit: [], attack: [], gen: [] },
+    VM: { hit: [], attack: [], gen: [] },
+    IG: { hit: [], attack: [], gen: [] },
+    L: { hit: [], attack: [], gen: [] },
   };
 
-  const sceneImagesDir = path.join(__dirname, "../scene-images");
+  const imageContext = require.context("../scene-images", true, /\.jpg$/i);
+  const files = imageContext.keys();
 
-  const subDirs = ["SP", "AT", "VM", "IG", "L"];
+  for (let i = 0; i < files.length; i += 1) {
+    const file = files[i];
+    const filePath = imageContext(file);
+    const fileName = file.toLowerCase();
 
-  for (let i = 0; i < subDirs.length; i += 1) {
-    const subDir = subDirs[i];
-    const subDirPath = path.join(sceneImagesDir, subDir);
+    const subDir = file.split("/")[1].toUpperCase();
+    console.log("subDir:", subDir);
 
-    const files = fs.readdirSync(subDirPath);
-
-    for (let j = 0; j < files.length; j += 1) {
-      const file = files[j];
-      if (file.endsWith(".jpg")) {
-        const filePath = path.join(subDirPath, file);
-        const fileName = file.toLowerCase();
-
-        if (fileName.includes("hit")) {
-          imageRefs[subDir.toLowerCase()].hit.push(filePath);
-        } else if (fileName.includes("attack")) {
-          imageRefs[subDir.toLowerCase()].attack.push(filePath);
-        } else if (fileName.includes("gen")) {
-          imageRefs[subDir.toLowerCase()].gen.push(filePath);
-        }
-      }
+    if (fileName.includes("hit")) {
+      imageRefs[subDir].hit.push(filePath);
+    } else if (fileName.includes("attack")) {
+      imageRefs[subDir].attack.push(filePath);
+    } else if (fileName.includes("gen")) {
+      imageRefs[subDir].gen.push(filePath);
     }
   }
 
--- src/modules/gameLog.js
+++ src/modules/gameLog.js
@@ -1,3 +1,5 @@
+import imageLoader from "../helpers/imageLoader";
+
 const gameLog = (() => {
   // Ref to log text
   const logText = document.querySelector(".log-text");
@@ -14,44 +16,10 @@ const gameLog = (() => {
     }
   };
 
-  // Set the scene image in the log
-  // Loads scene images for use
-  const sceneImages = { sp: {}, at: {}, vm: {}, ig: {}, l: {} };
+  // Log scene handling
+  let sceneImages = null;
   const loadScenes = () => {
-    // Urls for all scene images
-    const urls = {
-      sp: {
-        gen: [
-          "../scene-images/Sentinel Probe/sp_gen1.jpg",
-          "../scene-images/Sentinel Probe/sp_gen2.jpg",
-          "../scene-images/Sentinel Probe/sp_gen3.jpg",
-          "../scene-images/Sentinel Probe/sp_gen4.jpg",
-        ],
-        attack: [
-          "../scene-images/Sentinel Probe/sp_attack1.jpg",
-          "../scene-images/Sentinel Probe/sp_attack2.jpg",
-          "../scene-images/Sentinel Probe/sp_attack3.jpg",
-          "../scene-images/Sentinel Probe/sp_attack4.jpg",
-        ],
-        hit: [
-          "../scene-images/Sentinel Probe/sp_hit1.jpg",
-          "../scene-images/Sentinel Probe/sp_hit2.jpg",
-          "../scene-images/Sentinel Probe/sp_hit3.jpg",
-        ],
-      },
-    };
-
-    // Load sentinel probe images for use
-    Object.keys(urls.sp).forEach((key) => {
-      // Set the property that holds imgs to array
-      sceneImages.sp[key] = [];
-      // Add imgs to that array
-      urls.sp[key].forEach((url) => {
-        const image = new Image();
-        image.src = url;
-        sceneImages.sp[key].push(image);
-      });
-    });
+    sceneImages = imageLoader();
   };
 
   // Sets the scene image based on params passed

10e9bccbb059438745368cc9d63296cb5aef766b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-17 13:50:43 -0400

    Scan text and log if user attack

--- src/modules/gameLog.js
+++ src/modules/gameLog.js
@@ -1,8 +1,35 @@
 import imageLoader from "../helpers/imageLoader";
 
-const gameLog = (() => {
+const gameLog = ((aiGameboard, userGameboard, userName = "User") => {
   // Ref to log text
   const logText = document.querySelector(".log-text");
+
+  // Log scene handling
+  let sceneImages = null;
+  // Method for loading scene images. Must be run once in game manager.
+  const loadScenes = () => {
+    sceneImages = imageLoader();
+  };
+
+  // Sets the scene image based on params passed
+  const setScene = () => {
+    // Set the text to lowercase for comparison
+    const logLower = logText.textContent.toLowerCase();
+
+    // Set the image when you attack based on remaining ships
+    if (
+      logLower.includes(userName.toLowerCase()) &&
+      logLower.includes("attacks")
+    ) {
+      console.log("USER ATTACK IMG");
+    }
+
+    // Set the image when an AI ship sinks
+
+    // Set the image when your ship is hit or sinks
+
+    // Set the image when there is a miss to gen of remaining ships
+  };
 
   // Erase the log text
   const erase = () => {
@@ -14,21 +41,11 @@ const gameLog = (() => {
     if (stringToAppend) {
       logText.innerHTML += `\n${stringToAppend.toString()}`;
     }
+
+    setScene();
   };
 
-  // Log scene handling
-  let sceneImages = null;
-  // Method for loading scene images. Must be run once in game manager.
-  const loadScenes = () => {
-    sceneImages = imageLoader();
-  };
-
-  // Sets the scene image based on params passed
-  const setScene = () => {
-    // Set the img based on log text
-  };
-
-  return { erase, append, loadScenes };
+  return { erase, append, setScene, loadScenes };
 })();
 
 export default gameLog;

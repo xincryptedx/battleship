df54247a4a7abdf16b120f0b26be3e7aa37b6f56
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-17 11:21:46 -0400

    Add objects for storing urls and loading imgs

--- src/modules/gameLog.js
+++ src/modules/gameLog.js
@@ -1,3 +1,35 @@
+// Loads scene images for use
+const sceneImages = { sp: {}, at: {}, vm: {}, ig: {}, l: {} };
+const loadScenes = () => {
+  // Urls for all scene images
+  const urls = {
+    sp: {
+      gen: [
+        "../scene-images/Sentinel Probe/sp_gen1",
+        "../scene-images/Sentinel Probe/sp_gen2",
+        "../scene-images/Sentinel Probe/sp_gen3",
+        "../scene-images/Sentinel Probe/sp_gen4",
+      ],
+      attack: [
+        "../scene-images/Sentinel Probe/sp_attack1",
+        "../scene-images/Sentinel Probe/sp_attack2",
+        "../scene-images/Sentinel Probe/sp_attack3",
+        "../scene-images/Sentinel Probe/sp_attack4",
+      ],
+      hit: [
+        "../scene-images/Sentinel Probe/sp_hit1",
+        "../scene-images/Sentinel Probe/sp_hit2",
+        "../scene-images/Sentinel Probe/sp_hit3",
+      ],
+    },
+  };
+
+  // Load sentinel probe images for use
+  Object.keys(urls.sp).forEach((key) => {
+    // stuff
+  });
+};
+
 const gameLog = (() => {
   // Ref to log text
   const logText = document.querySelector(".log-text");
@@ -12,6 +44,11 @@ const gameLog = (() => {
     if (stringToAppend) {
       logText.innerHTML += `\n${stringToAppend.toString()}`;
     }
+  };
+
+  // Sets the scene image based on params passed
+  const scene = (unit, state) => {
+    // Set the img based on params
   };
 
   return { erase, append };

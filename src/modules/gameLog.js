725f69acd0ffe63306b6b391e2d4ee19c72864fc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-17 11:43:01 -0400

    Move load method into main module

--- src/modules/gameLog.js
+++ src/modules/gameLog.js
@@ -1,35 +1,3 @@
-// Loads scene images for use
-const sceneImages = { sp: {}, at: {}, vm: {}, ig: {}, l: {} };
-const loadScenes = () => {
-  // Urls for all scene images
-  const urls = {
-    sp: {
-      gen: [
-        "../scene-images/Sentinel Probe/sp_gen1",
-        "../scene-images/Sentinel Probe/sp_gen2",
-        "../scene-images/Sentinel Probe/sp_gen3",
-        "../scene-images/Sentinel Probe/sp_gen4",
-      ],
-      attack: [
-        "../scene-images/Sentinel Probe/sp_attack1",
-        "../scene-images/Sentinel Probe/sp_attack2",
-        "../scene-images/Sentinel Probe/sp_attack3",
-        "../scene-images/Sentinel Probe/sp_attack4",
-      ],
-      hit: [
-        "../scene-images/Sentinel Probe/sp_hit1",
-        "../scene-images/Sentinel Probe/sp_hit2",
-        "../scene-images/Sentinel Probe/sp_hit3",
-      ],
-    },
-  };
-
-  // Load sentinel probe images for use
-  Object.keys(urls.sp).forEach((key) => {
-    // stuff
-  });
-};
-
 const gameLog = (() => {
   // Ref to log text
   const logText = document.querySelector(".log-text");
@@ -46,12 +14,52 @@ const gameLog = (() => {
     }
   };
 
+  // Set the scene image in the log
+  // Loads scene images for use
+  const sceneImages = { sp: {}, at: {}, vm: {}, ig: {}, l: {} };
+  const loadScenes = () => {
+    // Urls for all scene images
+    const urls = {
+      sp: {
+        gen: [
+          "../scene-images/Sentinel Probe/sp_gen1",
+          "../scene-images/Sentinel Probe/sp_gen2",
+          "../scene-images/Sentinel Probe/sp_gen3",
+          "../scene-images/Sentinel Probe/sp_gen4",
+        ],
+        attack: [
+          "../scene-images/Sentinel Probe/sp_attack1",
+          "../scene-images/Sentinel Probe/sp_attack2",
+          "../scene-images/Sentinel Probe/sp_attack3",
+          "../scene-images/Sentinel Probe/sp_attack4",
+        ],
+        hit: [
+          "../scene-images/Sentinel Probe/sp_hit1",
+          "../scene-images/Sentinel Probe/sp_hit2",
+          "../scene-images/Sentinel Probe/sp_hit3",
+        ],
+      },
+    };
+
+    // Load sentinel probe images for use
+    Object.keys(urls.sp).forEach((key) => {
+      // Set the property that holds imgs to array
+      sceneImages.sp[key] = [];
+      // Add imgs to that array
+      urls.sp[key].forEach((url) => {
+        const image = new Image();
+        image.src = url;
+        sceneImages.sp[key].push(image);
+      });
+    });
+  };
+
   // Sets the scene image based on params passed
   const scene = (unit, state) => {
     // Set the img based on params
   };
 
-  return { erase, append };
+  return { erase, append, loadScenes };
 })();
 
 export default gameLog;

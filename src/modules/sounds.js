40415178c9b0f85bbfedec08e563d59aa4215211
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-18 14:04:24 -0400

    Write and return method for playing hit

--- src/modules/sounds.js
+++ src/modules/sounds.js
@@ -2,6 +2,18 @@ import hitSound from "../Sound Effects/explosion.mp3";
 import missSound from "../Sound Effects/miss.mp3";
 import attackSound from "../Sound Effects/laser.mp3";
 
-const sounds = () => {};
+const attackAudio = new Audio(attackSound);
+const hitAudio = new Audio(hitSound);
+const missAudio = new Audio(missSound);
+
+const sounds = () => {
+  const playHit = () => {
+    // Reset audio to beginning and play it
+    attackAudio.currentTime = 0;
+    attackAudio.play();
+  };
+
+  return { playHit };
+};
 
 export default sounds;

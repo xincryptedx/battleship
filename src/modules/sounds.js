b46efb36d5dfb50dc531c860e451f582b7ee90d0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-18 13:59:44 -0400

    Add module skeleton for sounds

---
+++ src/modules/sounds.js
@@ -0,0 +1,7 @@
+import hitSound from "../Sound Effects/explosion.mp3";
+import missSound from "../Sound Effects/miss.mp3";
+import attackSound from "../Sound Effects/laser.mp3";
+
+const sounds = () => {};
+
+export default sounds;

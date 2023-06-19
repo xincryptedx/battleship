78af591fbd7615df6fde9e01b5fefc0835c16592
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-19 15:23:07 -0400

    Move init logic to index for now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 /* eslint-disable no-unused-vars */
+// Import style sheets
 import "./style/reset.css";
 import "./style/style.css";
-import gameManager from "./modules/gameManager";
 
-// Initialize modules
-gameManager();
+// Import modules
+import gameManager from "./modules/gameManager";
+import Player from "./factories/Player";
+import canvasAdder from "./helpers/canvasAdder";
+import webInterface from "./modules/webInterface";
+import placeAiShips from "./helpers/placeAiShips";
+import gameLog from "./modules/gameLog";
+
+// #region Loading/Init
+// Load scene images for game log
+gameLog.loadScenes();
+
+// Initialization of Player objects for user and AI
+const userPlayer = Player();
+const aiPlayer = Player();
+userPlayer.gameboard.rivalBoard = aiPlayer.gameboard;
+aiPlayer.gameboard.rivalBoard = userPlayer.gameboard;
+userPlayer.gameboard.isAi = false;
+aiPlayer.gameboard.isAi = true;
+
+// Set gameLog user game board for accurate scenes
+gameLog.setUserGameboard(userPlayer.gameboard);
+// Init game log scene img
+gameLog.initScene();
+
+// Initialize the web interface with gameboards
+const webInt = webInterface(userPlayer.gameboard, aiPlayer.gameboard);
+// Add the canvas objects now that gameboards are created
+const canvases = canvasAdder(userPlayer.gameboard, aiPlayer.gameboard, webInt);
+// Add canvases to gameboards
+userPlayer.gameboard.canvas = canvases.userCanvas;
+aiPlayer.gameboard.canvas = canvases.aiCanvas;
+
+// #endregion
+
+// Add ai ships
+placeAiShips(1, aiPlayer.gameboard);
--- src/modules/gameManager.js
+++ src/modules/gameManager.js
@@ -8,49 +8,12 @@ import gameLog from "./gameLog";
    required objects, iterating through turns, reporting game outcome when
    a player loses, and restart the game */
 const gameManager = () => {
-  // #region Loading/Init
-  // Load scene images for game log
-  gameLog.loadScenes();
-
-  // Initialization of Player objects for user and AI
-  const userPlayer = Player();
-  const aiPlayer = Player();
-  userPlayer.gameboard.rivalBoard = aiPlayer.gameboard;
-  aiPlayer.gameboard.rivalBoard = userPlayer.gameboard;
-  userPlayer.gameboard.isAi = false;
-  aiPlayer.gameboard.isAi = true;
-
-  // Set gameLog user game board for accurate scenes
-  gameLog.setUserGameboard(userPlayer.gameboard);
-  // Init game log scene img
-  gameLog.initScene();
-
-  // Initialize the web interface with gameboards
-  const webInt = webInterface(userPlayer.gameboard, aiPlayer.gameboard);
-  // Add the canvas objects now that gameboards are created
-  const canvases = canvasAdder(
-    userPlayer.gameboard,
-    aiPlayer.gameboard,
-    webInt
-  );
-  // Add canvases to gameboards
-  userPlayer.gameboard.canvas = canvases.userCanvas;
-  aiPlayer.gameboard.canvas = canvases.aiCanvas;
-
-  // #endregion
-
-  // Add ai ships
-  placeAiShips(1, aiPlayer.gameboard);
   /* Method to determine if game is over after every turn. Checks allSunk on rival gameboard 
      and returns true or false */
-
   /* Method that flips a virtual coin to determine who goes first, and sets that
      player object to an active player variable */
-
   // Method for ending the game by reporting results
-
   // Method for restarting the game
-
   /* Iterate between players for attacks - if above method doesn't return true, check the
      active player and query them for their move. If above method is true, call method
      for ending the game, then method for restarting the game.
@@ -66,7 +29,6 @@ const gameManager = () => {
      -For AI use AI module's query method and pass in relevant parameters. AI module does its
      magic and returns a position. Then use gameboard.rivalboard.receivedAttack to make and 
      confirm the attack similar to the users attacks */
-
   // The logic of the game manager and how the web interface responds to it will remain
   // separated by using a pubsub module
 };

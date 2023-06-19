aa48711c0a451ca9f5ed0f47f0b10810fc64fcf9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-19 17:05:48 -0400

    Use the gm to access rival board

--- src/helpers/aiAttack.js
+++ src/helpers/aiAttack.js
@@ -1,28 +1,8 @@
 // This helper will look at current hits and misses and then return an attack
-const aiAttack = (rivalBoard, gm) => {
+const aiAttack = (gm) => {
   const gridHeight = 10;
   const gridWidth = 10;
-  const { hits, misses } = rivalBoard;
   let attackCoords = [];
-
-  // Method to determine if cell has a hit or miss in it
-  const alreadyAttacked = (cellCoordinates) => {
-    let attacked = false;
-
-    hits.forEach((hit) => {
-      if (cellCoordinates[0] === hit[0] && cellCoordinates[1] === hit[1]) {
-        attacked = true;
-      }
-    });
-
-    misses.forEach((miss) => {
-      if (cellCoordinates[0] === miss[0] && cellCoordinates[1] === miss[1]) {
-        attacked = true;
-      }
-    });
-
-    return attacked;
-  };
 
   // Method for returning random attack
   const randomAttack = () => {
@@ -33,7 +13,9 @@ const aiAttack = (rivalBoard, gm) => {
 
   // Try a random attack that has not been yet tried
   randomAttack();
-  while (alreadyAttacked(attackCoords)) {
+  console.log(gm);
+  while (gm.userBoard.alreadyAttacked(attackCoords)) {
+    console.log(attackCoords);
     randomAttack();
   }
 

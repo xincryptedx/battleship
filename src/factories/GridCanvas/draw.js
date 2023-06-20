f0e0694a2afa86ce77c91876929725876aac2c4c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-20 16:45:40 -0400

    Add method for dawing grid lines

--- src/factories/GridCanvas/draw.js
+++ src/factories/GridCanvas/draw.js
@@ -1,5 +1,30 @@
 const draw = () => {
-  // Methods for drawing to canvas
+  // Draws the grid lines
+  const lines = (context, canvasX, canvasY) => {
+    // Draw grid lines
+    const gridSize = Math.min(canvasX, canvasY) / 10;
+    const lineColor = "black";
+    context.strokeStyle = lineColor;
+    context.lineWidth = 1;
+
+    // Draw vertical lines
+    for (let x = 0; x <= canvasX; x += gridSize) {
+      context.beginPath();
+      context.moveTo(x, 0);
+      context.lineTo(x, canvasY);
+      context.stroke();
+    }
+
+    // Draw horizontal lines
+    for (let y = 0; y <= canvasY; y += gridSize) {
+      context.beginPath();
+      context.moveTo(0, y);
+      context.lineTo(canvasX, y);
+      context.stroke();
+    }
+  };
+
+  return { lines };
 };
 
 export default draw;

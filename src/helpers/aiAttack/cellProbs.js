2fc11067ff06556a02d0f2493eb037d7430b2658
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-21 15:11:54 -0400

    Add module for determining cell hit probabilities

---
+++ src/helpers/aiAttack/cellProbs.js
@@ -0,0 +1,7 @@
+const cellProbs = () => {
+  // Method that defines prior probabilities based on Alemi weights
+  // Method that updates probabilities based on hits, misses, and remaining ships' lengths
+  // Method for normalizing the probabilities
+};
+
+export default cellProbs;

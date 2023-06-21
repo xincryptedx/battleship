1c16540de4ac4963985bd76e71ab47125bad307d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-21 15:55:20 -0400

    Write pseudocode for initializing board

--- src/helpers/aiAttack/cellProbs.js
+++ src/helpers/aiAttack/cellProbs.js
@@ -1,7 +1,15 @@
 const cellProbs = () => {
-  // Method that defines prior probabilities based on Alemi weights
+  // Method that creates board and defines initial probabilities
+  const createBoard = () => {
+    // Create the board
+    // Assign initial probabilities based on Alemi's theory (0.08 in corners, 0.2 in 4 center cells)
+    // Adjust the weights based on Barry's theory (if board is checker board, prefer one color)
+    // Optionally display the output in the console
+    // Return the initialized board
+  };
   // Method that updates probabilities based on hits, misses, and remaining ships' lengths
   // Method for normalizing the probabilities
+  //
 };
 
 export default cellProbs;

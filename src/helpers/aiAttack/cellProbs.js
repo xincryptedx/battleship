c79ac80259b171465434414f8ab5729269bb89e9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-21 16:03:35 -0400

    Create the board and log the results

--- src/helpers/aiAttack/cellProbs.js
+++ src/helpers/aiAttack/cellProbs.js
@@ -1,15 +1,23 @@
 const cellProbs = () => {
   // Method that creates board and defines initial probabilities
   const createBoard = () => {
-    // Create the board
+    // Create the board. It is a 10x10 grid of cells.
+    const board = [];
+    for (let i = 0; i < 10; i += 1) {
+      board.push(Array(10).fill(0));
+    }
     // Assign initial probabilities based on Alemi's theory (0.08 in corners, 0.2 in 4 center cells)
     // Adjust the weights based on Barry's theory (if board is checker board, prefer one color)
     // Optionally display the output in the console
+    for (let row = 0; row < 10; row += 1) {
+      for (let col = 0; col < 10; col += 1) {
+        console.log(`Cell (${row}, ${col}) - Probability: ${board[row][col]}`);
+      }
+    }
     // Return the initialized board
   };
   // Method that updates probabilities based on hits, misses, and remaining ships' lengths
   // Method for normalizing the probabilities
-  //
 };
 
 export default cellProbs;

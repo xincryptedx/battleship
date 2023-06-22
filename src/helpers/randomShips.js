9366138c7950e3c1178dad9bf4af0fb733b55aef
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-22 13:45:39 -0400

    Make recursive and add exit condition

--- src/helpers/randomShips.js
+++ src/helpers/randomShips.js
@@ -1,11 +1,16 @@
 const randomShips = (gameboard, gridX, gridY) => {
+  // Exit from recursion
+  if (gameboard.ships.length > 4) return;
   // Get random placement
   const x = Math.floor(Math.random() * gridX);
   const y = Math.floor(Math.random() * gridY);
   const direction = Math.round(Math.random());
-  // Try the placement
 
+  // Try the placement
   gameboard.addShip([x, y], direction);
+
+  // Keep doing it until all ships are placed
+  randomShips(gameboard, gridX, gridY);
 };
 
 export default randomShips;

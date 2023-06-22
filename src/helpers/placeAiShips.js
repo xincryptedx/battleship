26d1d2fdbdc62928e555463674791c698cdc43fd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-22 13:49:25 -0400

    Implement new helper

--- src/helpers/placeAiShips.js
+++ src/helpers/placeAiShips.js
@@ -1,34 +1,20 @@
+import randomShips from "./randomShips";
+
 // This helper will attempt to add ships to the ai gameboard in a variety of ways for varying difficulty
 const placeAiShips = (passedDiff, aiGameboard) => {
   // Grid size
   const gridHeight = 10;
   const gridWidth = 10;
 
-  // Copy of the ai ships array and method to get it
-  const aiShips = aiGameboard.ships;
-
-  // Place a ship randomly
-  const placeRandomShip = () => {
-    // Get random placement
-    const x = Math.floor(Math.random() * gridWidth);
-    const y = Math.floor(Math.random() * gridHeight);
-    const direction = Math.round(Math.random());
-    // Try the placement
-
-    aiGameboard.addShip([x, y], direction);
-  };
-  // Place a ship along edges until one successfully placed
-  // Place a ship based on quadrant
+  // Place a ship along edges until one successfully placed ?
+  // Place a ship based on quadrant ?
 
   // Combine placement tactics to create varying difficulties
-  const placeShips = async (difficulty) => {
+  const placeShips = (difficulty) => {
     // Totally random palcement
-    if (difficulty === 1 && aiShips.length <= 4) {
-      // Try random placement
-      placeRandomShip();
-
-      // Recursively call fn until ships placed
-      placeShips(difficulty);
+    if (difficulty === 1) {
+      // Place ships randomly
+      randomShips(aiGameboard, gridWidth, gridHeight);
     }
   };
 

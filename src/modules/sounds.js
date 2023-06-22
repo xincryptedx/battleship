0f251887e0bf8a6edf295fb27fa35f0b9b26b1dc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-06-22 14:01:40 -0400

    Add mute flag to sounds

--- src/modules/sounds.js
+++ src/modules/sounds.js
@@ -7,19 +7,25 @@ const hitAudio = new Audio(hitSound);
 const missAudio = new Audio(missSound);
 
 const sounds = () => {
+  // Flag for muting
+  const isMuted = false;
+
   const playHit = () => {
+    if (isMuted) return;
     // Reset audio to beginning and play it
     hitAudio.currentTime = 0;
     hitAudio.play();
   };
 
   const playMiss = () => {
+    if (isMuted) return;
     // Reset audio to beginning and play it
     missAudio.currentTime = 0;
     missAudio.play();
   };
 
   const playAttack = () => {
+    if (isMuted) return;
     // Reset audio to beginning and play it
     attackAudio.currentTime = 0;
     attackAudio.play();
